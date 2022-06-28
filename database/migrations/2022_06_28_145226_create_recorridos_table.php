1922bfe3ecc30ae8dd02338db05ff27b7ca2c205
wilmerzel19 <wilmerzel19@gmail.com>
2022-06-28 08:53:11 -0700

    Integracion de breez,tablas en el proyecto

---
+++ database/migrations/2022_06_28_145226_create_recorridos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recorridos', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->string('km');
+            $table->string('recorrido');
+            $table->string('litros');
+            $table->string('persona_id');
+            $table->string('unidad_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recorridos');
+    }
+};
